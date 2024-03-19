41eb51b3516a18a28782605f835413b59a8ef78c
DEVINx34 <devinx34@gmail.com>
2024-03-20 02:22:13 +0530

    added methods

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import pandas as pd
 
 class Hotel:
-    def __init__(self, id):
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
         pass
     def book(self):
-        pass
+        """Books a hotel by changing its availability"""
+        df.loc[df['id'] == self.hotel_id, "available"] = "no"
+        df.to_csv('hotel.csv', index=False)
 
     def available(self):
-        pass
+        """Checks if the hotel is avaialble"""
+        availability = df.loc[df['id'] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+
+        else:
+            return False
 
 
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
+        pass
     def generate(self):
         pass
 
 
 df = pd.read_csv("hotels.csv")
 print(df)
-id = int(input("Enter the id of Hotel: "))
-hotel = Hotel(id)
+hotel_ID = int(input("Enter the id of Hotel: "))
+hotel = Hotel(hotel_ID)
 
 if hotel.available():
     hotel.book()
