d9cef18e7f6af6804778d754e4230d83a11eacf6
DEVINx34 <devinx34@gmail.com>
2024-03-20 02:00:52 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+class Hotel:
+    def __init__(self, id):
+        pass
+    def book(self):
+        pass
+
+    def available(self):
+        pass
+
+
+class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+    def generate(self):
+        pass
+
+
+df = pd.read_csv("hotels.csv")
+print(df)
+id = int(input("Enter the id of Hotel: "))
+hotel = Hotel(id)
+
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(name, hotel)
+    reservation_ticket.generate()
+
+else:
+    print("Hotel is not free.")
