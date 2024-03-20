fed7d55bc7bee70954c12405876812994c1b6a56
DEVINx34 <devinx34@gmail.com>
2024-03-21 01:46:16 +0530

    worked on Reservation ticket class and book method

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
+
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
-        pass
+        self.name = df.loc[df['id'] == self.hotel_id, "name"].squeeze()
+
     def book(self):
         """Books a hotel by changing its availability"""
         df.loc[df['id'] == self.hotel_id, "available"] = "no"
@@ -21,12 +23,20 @@ class Hotel:
 
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
     def generate(self):
-        pass
+        content = f"""
+        Thank you for your reservation
+        Here is your booking data:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}"""
+
+        return content
 
 
-df = pd.read_csv("hotels.csv")
+df = pd.read_csv("hotel.csv")
 print(df)
 hotel_ID = int(input("Enter the id of Hotel: "))
 hotel = Hotel(hotel_ID)
@@ -35,7 +45,7 @@ if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
     reservation_ticket = ReservationTicket(name, hotel)
-    reservation_ticket.generate()
+    print(reservation_ticket.generate())
 
 else:
     print("Hotel is not free.")
